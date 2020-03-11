f874b79bc7849e82527a13a4f0d6b4aaabebae0c
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-03-11 19:48:11 +0100

    wip

---
+++ src/components/SirCalculator.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SirCalculator() {
+    return (
+        <div className="text-red-500">
+            this is something
+        </div>
+    );
+}
