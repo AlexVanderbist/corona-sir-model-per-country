94c6d1a6a294b1304140c94215c06bb445fec543
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-03-12 16:45:31 +0100

    wip

--- src/components/StatsRow.tsx
+++ src/components/StatsRow.tsx
@@ -10,15 +10,15 @@ export default function StatsRow({location: {id, country, daysSinceFirstInfectio
 
     return (
         <tr key={id}>
-            <td>{rank}</td>
-            <td className="text-left">{country}</td>
-            <td style={tabularNums} className="text-right">{confirmed}</td>
-            <td style={tabularNums} className="text-right">{daysSinceFirstInfection}</td>
-            <td style={tabularNums} className="text-right">{beta?.toFixed(2)}</td>
-            <td>{peakInfectionDay}</td>
-            <td>{(peakInfected / population).toFixed(2)}%</td>
-            <td>{Math.round(peakInfected)}</td>
-            <td style={tabularNums} className="text-right">{population}</td>
+            <td className="px-3">{rank}</td>
+            <td className="text-left px-3">{country}</td>
+            <td style={tabularNums} className="text-right px-3">{confirmed}</td>
+            <td style={tabularNums} className="text-right px-3">{daysSinceFirstInfection}</td>
+            <td style={tabularNums} className="text-right px-3">{beta?.toFixed(2)}</td>
+            <td className="px-3">{peakInfectionDay}</td>
+            <td className="px-3">{(peakInfected / population).toFixed(2)}%</td>
+            <td className="px-3">{Math.round(peakInfected)}</td>
+            <td style={tabularNums} className="text-right px-3">{population}</td>
         </tr>
     );
 }
